810b32ae7e6c85d339ad94e5e9ac3ba296805e61
RumbleKAT <reki318@naver.com>
2021-04-04 20:43:06 +0900

    1.3.1 sample code

--- src/com/company/UserDao.java
+++ src/com/company/UserDao.java
@@ -4,21 +4,15 @@ import java.sql.*;
 
 public class UserDao {
 
-    String server = "localhost"; // MySQL 서버 주소
-    String database = "toby"; // MySQL DATABASE 이름
-    String user_name = "root"; //  MySQL 서버 아이디
-    String password = ""; // MySQL 서버 비밀번호
+    private SimpleConnectionMaker simpleConnectionMaker;
+
+    public UserDao(){
+        simpleConnectionMaker = new SimpleConnectionMaker();
+    }
 
     public void add(User user) throws ClassNotFoundException, SQLException{
         // 1.드라이버 로딩
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.err.println(" !! <JDBC 오류> Driver load 오류: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-        Connection c = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + "?useSSL=false&allowPublicKeyRetrieval=true", user_name, password);
+        Connection c = simpleConnectionMaker.makeNewConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values (?,?,?)");
 
         ps.setString(1,user.getId());
@@ -33,14 +27,7 @@ public class UserDao {
 
     public User get(String id) throws ClassNotFoundException, SQLException{
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.err.println(" !! <JDBC 오류> Driver load 오류: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-        Connection c = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + "?useSSL=false&allowPublicKeyRetrieval=true", user_name, password);
+        Connection c = simpleConnectionMaker.makeNewConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1,id);
 
@@ -57,4 +44,5 @@ public class UserDao {
 
         return user;
     }
+
 }
